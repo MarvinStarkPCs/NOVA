92e5921c1b9817aa2c46c95b9407a68d4c14c63a
MarvinStarkPCs <marvineduardosantosperez@gmail.com>
2025-08-25 06:20:39 -0500

    commit

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -13,7 +13,13 @@ class IndexController extends BaseController
         // Verifica si el usuario está logueado
         if ($session->get('login')) {
             // Redirige al usuario a la página principal
-            return redirect()->to('/admin/dashboard');
+             switch ($session->get('role_id')) {
+                case 1: // profesor
+                    return redirect()->to('/profesor/home');
+                case 2: // estudiante
+                    return redirect()->to('/estudiante/home');
+                case 3: // administrador
+                    return redirect()->to('/admin/home');}
         } else {
             // Muestra la vista de login si no está logueado
             return view('index');
